f971c2778f216d4bd57d3345f25e81dcc5a0bccb
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-04 17:32:44 +0530

    started with signature of init function

--- inc/beedb.h
+++ inc/beedb.h
@@ -1,4 +1,29 @@
 #ifndef BEEDB_H
 #define BEEDB_H
 
+typedef struct beedb beedb;
+struct beedb
+{
+	// path for database file
+	const char* database_file_name;
+
+	// attributes to access persistent store
+	mini_transaction_engine mtx;
+	page_access_methods mtx_pam;
+	page_modification_methods mtx_pmm;
+
+	// attributes to access volatile store
+	volatile_page_store vps;
+	page_access_methods vps_pam;
+	page_modification_methods vps_pmm;
+};
+
+void init_beedb(beedb* bdb, const char* database_file_name,
+		uint32_t page_id_width,
+		uint32_t page_size_mtx, uint32_t lsn_width, uint64_t bufferpool_frame_count, uint64_t wale_buffer_count,
+			uint64_t page_latch_wait_us, uint64_t page_lock_wait_us,
+			uint64_t checkpoint_period_us,
+		uint32_t page_size_vps,
+			uint64_t truncator_period_in_microseconds);
+
 #endif
