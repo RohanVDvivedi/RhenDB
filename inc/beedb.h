41fea39c4121df8de45a40910dc6d8cf041d0684
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-04 19:18:22 +0530

    added logic for initializing persistent store and volatile store

--- inc/beedb.h
+++ inc/beedb.h
@@ -14,8 +14,8 @@ struct beedb
 
 	// attributes to access persistent store
 	mini_transaction_engine mte;
-	page_access_methods mtx_pam;
-	page_modification_methods mtx_pmm;
+	page_access_methods mte_pam;
+	page_modification_methods mte_pmm;
 
 	// attributes to access volatile store
 	volatile_page_store vps;
@@ -25,10 +25,10 @@ struct beedb
 
 void init_beedb(beedb* bdb, const char* database_file_name,
 		uint32_t page_id_width,
-		uint32_t page_size_mtx, uint32_t lsn_width, uint64_t bufferpool_frame_count, uint64_t wale_buffer_count,
+		uint32_t page_size_mte, uint32_t lsn_width, uint64_t bufferpool_frame_count, uint64_t wale_buffer_count,
 			uint64_t page_latch_wait_us, uint64_t page_lock_wait_us,
 			uint64_t checkpoint_period_us,
 		uint32_t page_size_vps,
-			uint64_t truncator_period_in_microseconds);
+			uint64_t truncator_period_us);
 
 #endif
