6c6dd0f9338fc3bebcde4e2eb7990451431ed3f7
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-22 20:24:24 +0530

    added one more utility function to mvcc header

--- src/mvcc_header.c
+++ src/mvcc_header.c
@@ -5,3 +5,5 @@ tuple_def* get_mvcc_header_tuple_definition(uint8_t transaction_id_width);
 void read_mvcc_header(mvcc_header* mvcchdr_p, const void* mvcchdr_uval, const tuple_def* mvcchdr_tdef);
 
 void write_mvcc_header(void* mvcchdr_uval, const tuple_def* mvcchdr_tdef, const mvcc_header* mvcchdr_p);
+
+int update_mvcc_header(mvcc_header* mvcchdr_p, transaction_status (*get_transaction_status)(uint256 transaction_id));
