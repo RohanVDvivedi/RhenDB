c0d1a8d9e9ea595b9943e68d8f87e8bf3ebca095
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-23 23:05:56 +0530

    now we can fetch the status of the transaction using the hint bits or the transaction table

--- src/mvcc_header.c
+++ src/mvcc_header.c
@@ -6,4 +6,38 @@ void read_mvcc_header(mvcc_header* mvcchdr_p, const void* mvcchdr_uval, const tu
 
 void write_mvcc_header(void* mvcchdr_uval, const tuple_def* mvcchdr_tdef, const mvcc_header* mvcchdr_p);
 
-transaction_status fetch_status_for_transaction_id_with_hints(transaction_id_with_hints* transaction_id, transaction_status (*get_transaction_status)(uint256 transaction_id), int* were_hints_updated);
+transaction_status fetch_status_for_transaction_id_with_hints(transaction_id_with_hints* transaction_id, transaction_status (*get_transaction_status)(uint256 transaction_id), int* were_hints_updated)
+{
+	// first try and answer from hints if possible
+	if(transaction_id->is_committed)
+		return TX_COMMITTED;
+
+	if(transaction_id->is_aborted)
+		return TX_ABORTED;
+
+	// go to the table and try to fetch it
+	transaction_status status = get_transaction_status(transaction_id->transaction_id);
+
+	// update the hints
+	switch(status)
+	{
+		case TX_COMMITTED :
+		{
+			transaction_id->is_committed = 1;
+			(*were_hints_updated) = 1;
+			break;
+		}
+		case TX_ABORTED :
+		{
+			transaction_id->is_aborted = 1;
+			(*were_hints_updated) = 1;
+			break;
+		}
+		default :
+		{
+			break;
+		}
+	}
+
+	return status;
+}
