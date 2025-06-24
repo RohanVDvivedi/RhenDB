f7a0582e96fde0883fb75fd1445bf79c7a6b2088
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-24 22:59:13 +0530

    added tuple definition for the mvcc header that is to be added to prefix

--- src/mvcc_header.c
+++ src/mvcc_header.c
@@ -1,10 +1,41 @@
 #include<rondb/mvcc_header.h>
 
-tuple_def* get_mvcc_header_tuple_definition(uint8_t transaction_id_width);
+#include<stdlib.h>
 
-void read_mvcc_header(mvcc_header* mvcchdr_p, const void* mvcchdr_uval, const tuple_def* mvcchdr_tdef);
+tuple_def* get_mvcc_header_tuple_definition(uint8_t transaction_id_width)
+{
+	data_type_info* dti_p = malloc(sizeof_tuple_data_type_info(6));
 
-void write_mvcc_header(void* mvcchdr_uval, const tuple_def* mvcchdr_tdef, const mvcc_header* mvcchdr_p);
+	initialize_tuple_data_type_info(dti_p, "mvcc_header", 0, 0, 6);
+
+	strcpy(dti_p->containees[0].field_name, "is_xmin_committed");
+	dti_p->containees[0].al.type_info = BIT_FIELD_NON_NULLABLE[1];
+
+	strcpy(dti_p->containees[1].field_name, "is_xmin_aborted");
+	dti_p->containees[1].al.type_info = BIT_FIELD_NON_NULLABLE[1];
+
+	strcpy(dti_p->containees[2].field_name, "xmin");
+	dti_p->containees[2].al.type_info = LARGE_UINT_NON_NULLABLE[transaction_id_width];
+
+	strcpy(dti_p->containees[3].field_name, "is_xmax_committed");
+	dti_p->containees[3].al.type_info = BIT_FIELD_NON_NULLABLE[1];
+
+	strcpy(dti_p->containees[4].field_name, "is_xmax_aborted");
+	dti_p->containees[4].al.type_info = BIT_FIELD_NON_NULLABLE[1];
+
+	strcpy(dti_p->containees[5].field_name, "xmax");
+	dti_p->containees[5].al.type_info = LARGE_UINT_NULLABLE[transaction_id_width];
+
+	tuple_def* mvcchdr_def = malloc(sizeof(tuple_def));
+
+	initialize_tuple_def(mvcchdr_def, dti_p);
+
+	return mvcchdr_def;
+}
+
+void read_mvcc_header(mvcc_header* mvcchdr_p, const void* mvcchdr_uval, const tuple_def* mvcchdr_def);
+
+void write_mvcc_header(void* mvcchdr_uval, const tuple_def* mvcchdr_def, const mvcc_header* mvcchdr_p);
 
 transaction_status fetch_status_for_transaction_id_with_hints(transaction_id_with_hints* transaction_id, transaction_status (*get_transaction_status)(uint256 transaction_id), int* were_hints_updated)
 {
