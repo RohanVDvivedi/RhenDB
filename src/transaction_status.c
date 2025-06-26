192068527b43e49883fe41af117b43ec9c07cfc7
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-26 21:41:32 +0530

    added transaction status string to print it

---
+++ src/transaction_status.c
@@ -0,0 +1,7 @@
+#include<rondb/transaction_status.h>
+
+char const * const transaction_status_string[] = {
+	[TX_IN_PROGRESS] = "TX_IN_PROGRESS",
+	[TX_ABORTED] = "TX_ABORTED",
+	[TX_COMMITTED] = "TX_COMMITTED",
+};
