0a2d7adba4ca4eacfc5a3d2c6e06542f5106082a
RohanVDvivedi <rohandvivedi@gmail.com>
2025-08-30 01:45:03 +0530

    added waits entry

--- src/lock_manager.c
+++ src/lock_manager.c
@@ -6,28 +6,6 @@
 ** internal structures
 */
 
-typedef struct lock_entry lock_entry;
-struct lock_entry
-{
-	// transaction_id that holds the lock
-	uint256 transaction_id;
-
-	// this is the resource_type for the below resource_id
-	// this dictates what lock_modes can be used for this resource_type
-	// this implies it is same as the type of the lock to be used on this resource
-	// i.e. resource_type = lock_type, resulting into fixed usable number of lock_mode-s that you can acquire on it
-	uint32_t resource_type;
-
-	// the resource_id that is locked, it is atmost 16 bytes wide, i.e. 128 bits
-	uint8_t resource_id_size;
-	uint8_t resource_id[MAX_RESOURCE_ID_SIZE];
-
-	// lock mode is the mode of the lock, for instance a (resource_type ==) reader_writer_lock has 2 modes, READ_MODE and WRITE_MODE
-	uint32_t lock_mode;
-};
-
-#define MAX_SERIALIZED_LOCK_ENTRY_SIZE (sizeof(uint256) + sizeof(uint32_t) + MAX_RESOURCE_ID_SIZE + sizeof(uint32_t) + 2) // +2 for the size and offset of resource_id
-
 typedef struct active_transaction_entry active_transaction_entry;
 struct active_transaction_entry
 {
@@ -49,46 +27,60 @@ struct active_transaction_entry
 	bstnode embed_node; // embedded node for active transactions
 };
 
+// compare function using transaction_id for active_transaction_entry
+
+// hash function using transaction_id for active_transaction_entry
+
+#define MAX_SERIALIZED_LOCK_ENTRY_SIZE (sizeof(uint256) + sizeof(uint32_t) + MAX_RESOURCE_ID_SIZE + sizeof(uint32_t) + 2) // +2 for the size and offset of resource_id
+
+typedef struct lock_entry lock_entry;
+struct lock_entry
+{
+	// transaction_id that holds the lock
+	uint256 transaction_id;
+
+	// this is the resource_type for the below resource_id
+	// this dictates what lock_modes can be used for this resource_type
+	// this implies it is same as the type of the lock to be used on this resource
+	// i.e. resource_type = lock_type, resulting into fixed usable number of lock_mode-s that you can acquire on it
+	uint32_t resource_type;
+
+	// the resource_id that is locked, it is atmost 16 bytes wide, i.e. 128 bits
+	uint8_t resource_id_size;
+	uint8_t resource_id[MAX_RESOURCE_ID_SIZE];
+
+	// lock mode is the mode of the lock, for instance a (resource_type ==) reader_writer_lock has 2 modes, READ_MODE and WRITE_MODE
+	uint32_t lock_mode;
+};
+
+#define MAX_SERIALIZED_WAIT_ENTRY_SIZE (sizeof(uint256) + sizeof(uint256) + sizeof(uint32_t) + MAX_RESOURCE_ID_SIZE + sizeof(uint32_t) + 2) // +2 for the size and offset of resource_id
+
+typedef struct waits_entry waits_entry;
+struct waits_entry
+{
+	// transaction_id that is waiting for the lock
+	uint256 waiting_transaction_id;
+
+	// ON the transaction_id and the resource attributes below
+
+	// transaction_id that holds the lock
+	uint256 transaction_id;
+
+	// this is the resource_type for the below resource_id
+	// this dictates what lock_modes can be used for this resource_type
+	// this implies it is same as the type of the lock to be used on this resource
+	// i.e. resource_type = lock_type, resulting into fixed usable number of lock_mode-s that you can acquire on it
+	uint32_t resource_type;
+
+	// the resource_id that is locked, it is atmost 16 bytes wide, i.e. 128 bits
+	uint8_t resource_id_size;
+	uint8_t resource_id[MAX_RESOURCE_ID_SIZE];
+};
+
 // --
 
 /*
-** lock_table functions
+** utility functions
 */
-
-// insert to heap_table and both the indices
-
-// --
-
-/*
-** tx_index functions (transaction_id, resource_type, resource_id, lock_state, lock_mode) -> lock
-** to be used to release all locks, upon abort or commit, or release a specific lock
-*/
-
-// start a scan
-
-// get current
-
-// go next
-
-// remove from everywhere
-
-// destroy the scan
-
-// --
-
-/*
-** rt_index functions (resource_type, resource_id, lock_state, lock_mode) -> lock
-** to be used to check lock conflicts with other lock modes on the same resource_type and resource_id, for acquire, modify and deadlock detection
-*/
-
-// start a scan
-
-// get current
-
-// go next
-
-// remove from everywhere
-
-// destroy the scan
 
 // --
