15ba68eaa428a98c287c59f0dfc404ce8118a1a3
RohanVDvivedi <rohandvivedi@gmail.com>
2025-09-08 22:19:34 +0530

    acquisitions and waiting entries added correctly

--- test/test_lckmgr.c
+++ test/test_lckmgr.c
@@ -121,5 +121,12 @@ int main()
 
 	debug_print_lock_manager_tables(&lckmgr);
 
+	acquire_lock(&lckmgr, get_uint256(3), 0, RESOURCE_TYPE_0, 0, RW_DB_LOCK_W_MODE, 0);
+	acquire_lock(&lckmgr, get_uint256(3), 1, RESOURCE_TYPE_0, 1, RW_DB_LOCK_R_MODE, 0);
+	acquire_lock(&lckmgr, get_uint256(3), 2, RESOURCE_TYPE_1, 0, RW_DB_LOCK_W_MODE, 0);
+	acquire_lock(&lckmgr, get_uint256(3), 3, RESOURCE_TYPE_1, 1, RW_DB_LOCK_R_MODE, 0);
+
+	debug_print_lock_manager_tables(&lckmgr);
+
 	return 0;
 }
