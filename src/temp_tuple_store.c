4919c0463fa3125972df32ffa9014453cbd34d1e
RohanVDvivedi <rohandvivedi@gmail.com>
2025-09-23 21:18:12 +0530

    implemented basic functionality for tuple regions

--- src/temp_tuple_store.c
+++ src/temp_tuple_store.c
@@ -1,1 +1,28 @@
 #include<rhendb/temp_tuple_store.h>
+
+temp_tuple_store* get_new_temp_tuple_store(uint64_t spill_over_size);
+
+void delete_temp_tuple_store(temp_tuple_store* tts_p);
+
+int mmap_for_reading_tuple(temp_tuple_store* tts_p, tuple_region* tr_p, uint64_t offset, tuple_size_def* tpl_sz_d);
+
+int mmap_for_writing_tuple(temp_tuple_store* tts_p, tuple_region* tr_p, tuple_size_def* tpl_sz_d, uint32_t required_size);
+
+int finalize_written_tuple(temp_tuple_store* tts_p, tuple_region* tr_p);
+
+void unmap_for_tuple_region(temp_tuple_store* tts_p, tuple_region* tr_p);
+
+uint64_t curr_tuple_offset_for(tuple_region* tr_p)
+{
+	return tr_p->region_offset + (tr_p->tuple - tr_p->region_memory);
+}
+
+uint32_t curr_tuple_size_for(tuple_region* tr_p)
+{
+	return get_tuple_size_using_tuple_size_def(tr_p->tpl_sz_d, tr_p->tuple);
+}
+
+uint64_t next_tuple_offset_for(tuple_region* tr_p)
+{
+	return curr_tuple_offset_for(tr_p) + curr_tuple_size_for(tr_p);
+}
