5ebf1d54df88ceca563fa8dcd9f773a94dccfba2
RohanVDvivedi <rohandvivedi@gmail.com>
2025-09-27 22:41:27 +0530

    basic read function implemented

--- test/test_tts.c
+++ test/test_tts.c
@@ -7,6 +7,23 @@
 tuple_def tpl_d;
 data_type_info dti;
 
+void print_all_tuples(temp_tuple_store* tts_p)
+{
+	printf("\n\nprinting temp_tuple_store with %"PRIu64" tuples, and filled upto %"PRIu64"/%"PRIu64"\n\n", tts_p->tuple_count, tts_p->next_tuple_offset, tts_p->total_size);
+	uint64_t index = 0;
+	uint64_t offset = 0;
+	tuple_region tr = INIT_TUPLE_REGION;
+	while(mmap_for_reading_tuple(tts_p, &tr, offset, &(tpl_d.size_def)))
+	{
+		printf("tuple_index = %"PRIu64", tuple_offset = %"PRIu64", tuple_size = %"PRIu32"\n", index, offset, curr_tuple_size_for_tuple_region(&tr));
+		printf("\n\n");
+		offset = next_tuple_offset_for_tuple_region(&tr);
+		index++;
+	}
+	unmap_for_tuple_region(&tr);
+	printf("\n\n");
+}
+
 int main()
 {
 	printf("page_size = %"PRIu64"\n\n", sysconf(_SC_PAGE_SIZE));
@@ -17,6 +34,12 @@ int main()
 	print_tuple_def(&tpl_d);
 
 	temp_tuple_store* tts_p = get_new_temp_tuple_store(".");
+
+	// init complete
+
+	print_all_tuples(tts_p);
+
+	// deinit start
 
 	delete_temp_tuple_store(tts_p);
 	return 0;
