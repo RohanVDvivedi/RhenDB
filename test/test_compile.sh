cbeb0012542372dd1d42092380dc7032dd8515eb
RohanVDvivedi <rohandvivedi@gmail.com>
2025-09-27 22:14:53 +0530

    init and destroy functions added

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,4 +1,4 @@
 gcc ./test_mvcc.c -o test_mvcc.out -lrhendb -lmintxengine -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
 gcc ./test_ttbl.c -o test_ttbl.out -lrhendb -lmintxengine -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
 gcc ./test_lckmgr.c -o test_lckmgr.out -lrhendb -lmintxengine -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
-gcc ./test_tts.c -o test_ts.out -lrhendb -lmintxengine -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
+gcc ./test_tts.c -o test_tts.out -lrhendb -lmintxengine -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
--- test/test_tts.c
+++ test/test_tts.c
@@ -2,5 +2,9 @@
 
 int main()
 {
+	temp_tuple_store* tts_p = get_new_temp_tuple_store(".");
+
+
+	delete_temp_tuple_store(tts_p);
 	return 0;
 }
