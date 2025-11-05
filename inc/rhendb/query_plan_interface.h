4afc975b5905904587374b7b3a22a8533017af45
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-05 23:53:49 +0530

    major changes to the query_plan_interface.h

--- inc/rhendb/query_plan_interface.h
+++ inc/rhendb/query_plan_interface.h
@@ -8,21 +8,16 @@
 
 #include<rhendb/temp_tuple_store.h>
 
-#define MAX_TASKS_PER_OPERATOR 10000
-
 typedef enum operator_state operator_state;
 enum operator_state
 {
-	OPERATOR_QUEUED,	// queued in thread pool but not executing as of now
-	OPERATOR_RUNNING,	// executing right now
+	OPERATOR_QUEUED, // operator queued to thread pool
 
-	// notify_wake_up() for an operator only does something if the operator is in this state, this implies it is still running or paused completely
-	// but is waiting for someone to notify it, once an operator (or one of it's thread) is in this state, it can not come back without being notified
-	OPERATOR_WAITING_TO_BE_NOTIFIED,	// some thread of this operator is paused, because the source operators are paused OR waiting for a lock to be granted
+	OPERATOR_RUNNING, // executing right now
 
-	OPERATOR_TO_BE_KILLED,	// operator's was waiting for some resource and is marked to be killed sooner ar later, only next state it could go in is OPERATOR_KILLED
+	OPERATOR_WAITING, // operator waiting for some resource to be available
 
-	OPERATOR_KILLED,	// operator is not functional and has quit, this implies all the context is released before setting to this state
+	OPERATOR_KILLED, // no state transitions can occur for the operator after this state is reached, operator has released all the resources
 };
 
 typedef struct operator operator;
@@ -30,40 +25,36 @@ struct operator
 {
 	// below attributes are static and you do not need lock to access them
 
-	// operator_id is the id of the first worker in the group of threads of this operator
-	// you get 10000 such ids, it is believed you may never need more than 10000 threads for any one of your operators
+	// operator_id is the id of this operator
 	uint64_t operator_id;
 
 	// below attributes are not static and you need a lock to access them
 
 	pthread_mutex_t lock;		// global lock for the operator
 
-	pthread_cond_t wait_until_killed; // wait for any one to wait for the operator state to change to OPERATOR_KILLED state
+	pthread_cond_t wait_until_state_changes; // wait for the operator to change it's state
 
-	operator_state state;		// access the current state of the operator here
-	// you can not have different states for each of the threads of the operator
-	// for let's say a join operator joining two tables atmost can assign one operator_id to each of its input readers and only service wakeups when the correct operator_id is called for the notify_wake_up() call
-	// it is all upto you to design this
-	// for any external user an operator being in OPERATOR_WAITING_TO_BE_NOTIFIED, just means that it is desperate to run and needs someone to just give it a push
+	operator_state state;	// access the current state of the operator tasks here
 
-	void* input_params;			// pointer for the operator to store input params
+	void* input;			// pointer for the operator to store input params
 
-	void* context;				// context to be used by the operators, when they are paused, this helps them stay paused and then restart again when data is again available
-	// this could possibly be a local variable store for all your threads of this operators, so that these operator threads could sleep and wake up as they desire
+	void* context;			// context to be used by the operators, when they are waiting, this helps then restart again when data is again available
+							// this must hold the last position you were scanning at, so as to restore from that same old position, upon being relieved of the blocking from the resource (being locks in lock_table OR operator_buffer-s)
+
+	void* locals;			// local variables for use by the operator tasks, this may hold your scan structures like heap_table_iterator and bplus_tree_iterator
 
 	executor* thread_pool;		// thread_pool to be used by the operator, it would be a global cached thread pool for the scans and writers, but for joins, sorts and group_bys we will use a compute intensive fixed sized thread pool
 
-	void (*notify_wake_up)(operator* o, uint64_t operator_id); // the function that gets called to wake up a paused operator when new data is available OR when it could possibly have been unblocked for a lock
-	// here if operator_id is operator->operator_id if it is a broadcast to all the operators, else we are talking about one specific thread of this operator
-	// you can also have some operator that does it's entire work in this function itself (only shuffles between RUNNING and TO_BE_NOTIFIED states)
-	// OR an operator that just pushes it's execute function into the thread_pool upon notified to be called asynchronously
-	// each operator design is you to you to decide, how it should run synchronously or asynchronously
+	// called to put the operator in waiting state
+	// hold lock while calling this function and atomically transitioning from OPERATOR_RUNNING to OPERATOR_WAITING state
+	void (*store_locals_to_context)(operator* o);
 
-	// execute function will never be called by the external users, it is up to the operator designers to design how to use this function pointer
-	// it is essentially private to thethe operator just like the input_param and the context pointers
-	void (*execute)(operator* o); // the function that gets called inside the thread_pool
+	// called to put the operator in running from queued state
+	// hold lock while calling this function and atomically transitioning from OPERATOR_QUEUED to OPERATOR_RUNNING state
+	void (*restore_locals_from_context)(operator* o);
 
-	void (*clean_up)(operator* o); // the function must be called only after an operator reaches OPERATOR_KILLED state, OR 
+	// this is the function that runs for the operator, after it is set to OPERATOR_RUNNING state and atomically making calling it's restore_locals_from_context() function
+	void (*execute)(operator* o);
 };
 
 // public
@@ -83,16 +74,6 @@ int set_operator_state(operator* o, operator_state state);
 typedef struct operator_buffer operator_buffer;
 struct operator_buffer
 {
-	// this is the operator that pushes its produce into this operator buffer, this is static
-	// must never be NULL, there must be a procuder to an operator buffer
-	operator* producer;
-
-	// this is the operator that pops/consumes from this operator buffer, this is static
-	// must never be NULL, there must be a consumer to an operator buffer (there will always be a consumer the outer most operator must always be a writer to the user's TCP or STDIN file descriptors)
-	operator* consumer;
-
-	// there can be only 1 producer and 1 consumer for the operator_buffer, but these operators may be using multiple threads
-
 	pthread_mutex_t lock;		// global lock for the operator_buffer
 
 	pthread_cond_t wait;		// wait for the data here blockingly
@@ -100,8 +81,13 @@ struct operator_buffer
 	uint64_t tuple_stores_count;
 	linkedlist tuple_stores;	// temp_tuple_store produced by the operator gets stored here
 
-	int prohibit_usage;
-	// this implies an error in the system OR a request that the whole pipeline must collapse
+	// number of operator tasks producing to this operator buffer
+	uint64_t producers_count;
+
+	// number of operator tasks consuming from this operator buffer
+	uint64_t consumers_count;
+
+	// this counters can be incremented or decremented at will
 };
 
 // public
@@ -125,6 +111,9 @@ temp_tuple_store* pop_from_operator_buffer(operator_buffer* ob, uint64_t timeout
 typedef struct query_plan query_plan;
 struct query_plan
 {
+	// the transaction that this query belongs to
+	transaction* curr_tx;
+
 	// operators scans, writers, and also the joins, sorts and groupbys
 	uint64_t operators_count;
 	operator** operators;
