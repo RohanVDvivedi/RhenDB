55d6134a3180a259b24839431a8a2cb948e3c1ac
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-21 21:25:35 +0530

    built an interface to get the transaction_status

--- inc/rhendb/transaction_status.h
+++ inc/rhendb/transaction_status.h
@@ -1,6 +1,8 @@
 #ifndef TRANSACTION_STATUS_H
 #define TRANSACTION_STATUS_H
 
+#include<serint/large_uints.h>
+
 typedef enum transaction_status transaction_status;
 enum transaction_status
 {
@@ -11,5 +13,14 @@ enum transaction_status
 };
 
 extern char const * const transaction_status_string[];
+
+typedef struct transaction_status_getter transaction_status_getter;
+struct transaction_status_getter
+{
+	void* context;
+	transaction_status (*get_transaction_status)(void* context, uint256 transaction_id);
+};
+
+transaction_status get_transaction_status_for_transaction_id(transaction_status_getter* tsg, uint256 transaction_id);
 
 #endif
--- src/transaction_status.c
+++ src/transaction_status.c
@@ -5,3 +5,8 @@ char const * const transaction_status_string[] = {
 	[TX_ABORTED] = "TX_ABORTED",
 	[TX_COMMITTED] = "TX_COMMITTED",
 };
+
+transaction_status get_transaction_status_for_transaction_id(transaction_status_getter* tsg, uint256 transaction_id)
+{
+	return tsg->get_transaction_status(tsg->context, transaction_id);
+}
