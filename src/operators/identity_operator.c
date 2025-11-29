bd7b3ca66033864352a0d639ff170d73b4a84f54
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-30 03:23:42 +0530

    implemented skeleton for identity operator

--- src/operators/identity_operator.c
+++ src/operators/identity_operator.c
@@ -2,4 +2,35 @@
 
 #include<boompar/executor.h>
 
-void setup_identity_operator(operator* o, executor* thread_pool, operator_buffer* output, operator_buffer* input);
+typedef struct input_values input_values;
+struct input_values
+{
+	executor* thread_pool;
+	operator_buffer* output;
+	operator_buffer* input;
+};
+
+static void* execute(void* o_v);
+
+static void start_execution(operator* o)
+{
+	if(!submit_job_executor(((input_values*)(o->inputs))->thread_pool, execute, o, NULL, NULL, BLOCKING))
+	{
+		printf("FAILED TO SUBMIT INDENTITY OPERATOR\n");
+		exit(-1);
+	}
+}
+
+void setup_identity_operator(operator* o, executor* thread_pool, operator_buffer* output, operator_buffer* input)
+{
+	o->start_execution = start_execution;
+	o->operator_release_latches_and_store_context = OPERATOR_RELEASE_LATCH_NO_OP_FUNCTION;
+	o->free_resources = OPERATOR_FREE_RESOURCE_NO_OP_FUNCTION;
+
+	o->inputs = malloc(sizeof(input_values));
+	*((input_values*)(o->inputs)) = (input_values){
+		.thread_pool = thread_pool,
+		.output = output,
+		.input = input,
+	};
+}
