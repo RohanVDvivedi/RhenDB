bba3fb329fe13f65a7766f01afb101f60e578291
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-30 13:11:51 +0530

    identity operator implemented

--- src/operators/identity_operator.c
+++ src/operators/identity_operator.c
@@ -11,7 +11,54 @@ struct input_values
 
 static void execute(operator* o)
 {
-	// TODO
+	input_values* inputs = o->inputs;
+
+	dstring kill_reason = get_dstring_pointing_to_literal_cstring("completed_and_killed");
+
+	temp_tuple_store* tts = NULL;
+
+	while(1)
+	{
+		if(is_kill_signal_sent(o))
+			goto EXIT;
+
+		int no_more_data = 0;
+		tts = pop_from_operator_buffer(inputs->input, 0, 1000000, &no_more_data);
+
+		if(is_kill_signal_sent(o))
+			goto EXIT;
+
+		if(no_more_data)
+			goto EXIT;
+
+		if(tts != NULL)
+		{
+			int pushed = push_to_operator_buffer(inputs->output, o, tts);
+
+			if(is_kill_signal_sent(o))
+				goto EXIT;
+
+			if(!pushed)
+			{
+				kill_reason = get_dstring_pointing_to_literal_cstring("pushed_failed_from_identity_oerator_and_so_killed");
+				goto EXIT;
+			}
+
+			tts = NULL;
+		}
+	}
+
+	EXIT:
+	if(tts != NULL)
+	{
+		delete_temp_tuple_store(tts);
+		tts = NULL;
+	}
+
+	decrement_operator_buffer_consumers_count(inputs->input, 1);
+	decrement_operator_buffer_producers_count(inputs->output, 1);
+
+	mark_operator_self_killed(o, kill_reason);
 }
 
 void setup_identity_operator(operator* o, operator_buffer* output, operator_buffer* input)
