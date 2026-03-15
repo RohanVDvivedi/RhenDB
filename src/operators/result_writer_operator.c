9564be269e2328f506e07f372ba38247fa061894
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-15 20:48:16 +0530

    --

--- src/operators/result_writer_operator.c
+++ src/operators/result_writer_operator.c
@@ -18,23 +18,21 @@ static void trigger_execution(operator* o)
 
 	dstring kill_reason = get_dstring_pointing_to_literal_cstring("completed_and_killed");
 
-	interim_tuple_store* its = NULL;
-
 	int no_more_data = 0;
 
-	interim_tuple_store* its = consume_from_operator(inputs->input_operator, 500, &no_more_data);
+	interim_tuple_store* its_p = consume_from_operator(inputs->input_operator, 500, &no_more_data);
 	if(no_more_data)
 		mark_operator_self_killed(o, kill_reason);
 	if(can_not_proceed_for_execution_operator(o))
 		mark_operator_self_killed(o, kill_reason);
 
-	if(its != NULL)
+	if(its_p != NULL)
 	{
-		printf("\n\nprinting interim_tuple_store with %"PRIu64" tuples, and filled upto %"PRIu64"/%"PRIu64"\n\n", its->tuples_count, its->next_tuple_offset, its->total_size);
+		printf("\n\nprinting interim_tuple_store with %"PRIu64" tuples, and filled upto %"PRIu64"/%"PRIu64"\n\n", its_p->tuples_count, its_p->next_tuple_offset, its_p->total_size);
 		uint64_t index = 0;
 		uint64_t offset = 0;
-		tuple_region tr = INIT_TUPLE_REGION;
-		while(mmap_for_reading_tuple(its, &tr, offset, &(inputs->input_tuple_def->size_def), 0))
+		interim_tuple_region tr = INIT_INTERIM_TUPLE_REGION;
+		while(mmap_for_reading_tuple(its_p, &tr, offset, &(inputs->input_tuple_def->size_def), 0))
 		{
 			printf("tuple_index = %"PRIu64", tuple_offset = %"PRIu64", tuple_size = %"PRIu32"\n", index, offset, curr_tuple_size_for_interim_tuple_region(&tr));
 			print_tuple(tr.tuple, inputs->input_tuple_def);
@@ -42,11 +40,11 @@ static void trigger_execution(operator* o)
 			offset = next_tuple_offset_for_interim_tuple_region(&tr);
 			index++;
 		}
-		unmap_for_tuple_region(&tr);
+		unmap_for_interim_tuple_region(&tr);
 		printf("\n\n");
 
-		delete_temp_tuple_store(its);
-		its = NULL;
+		delete_interim_tuple_store(its_p);
+		its_p = NULL;
 	}
 
 	return;
