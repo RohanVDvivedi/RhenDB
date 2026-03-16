3ea8d71605df5881e931aed671950627cf9f6577
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-17 00:48:53 +0530

    minor comment added

--- inc/rhendb/function_compare.h
+++ inc/rhendb/function_compare.h
@@ -8,6 +8,7 @@
 int can_compare_datum_rhendb(const data_type_info* dti1, const data_type_info* dti2);
 
 // it only reads data from the ex_engine for the extended atributes, provision atleast 1 buffer for each one of this function calls
+// the ex_engine will/must be the min_tx_engine
 
 int compare_datum_rhendb(const datum* uval1, const data_type_info* dti1, const datum* uval2, const data_type_info* dti2, rage_engine* ex_engine, int* abort_error);
 
--- inc/rhendb/function_hash.h
+++ inc/rhendb/function_hash.h
@@ -6,6 +6,7 @@
 #include<tuplestore/datum.h>
 
 // it only reads data from the ex_engine for the extended atributes, provision atleast 1 buffer for each one of this function calls
+// the ex_engine will/must be the min_tx_engine
 uint64_t hash_datum_rhendb(const datum* uval, const data_type_info* dti, tuple_hasher* th, rage_engine* ex_engine, int* abort_error);
 
 #endif
