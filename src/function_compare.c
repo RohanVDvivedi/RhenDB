419108285b65e79540eb323362d4d393abb5556d
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-17 00:50:50 +0530

    4 functions in all

--- src/function_compare.c
+++ src/function_compare.c
@@ -4,5 +4,4 @@ int can_compare_datum_rhendb(const data_type_info* dti1, const data_type_info* d
 
 int compare_datum_rhendb(const datum* uval1, const data_type_info* dti1, const datum* uval2, const data_type_info* dti2, rage_engine* ex_engine, int* abort_error);
 
-// this function is added to skip the checks that ensure that the datums can be compared, so that such checks can be discarded
 int compare_datum2_rhendb(const datum* uval1, const datum* uval2, const data_type_info* dti, rage_engine* ex_engine, int* abort_error);
