11d9f81a81d42a233d7a59899fdd92704940182b
RohanVDvivedi <rohandvivedi@gmail.com>
2026-03-28 00:58:45 +0530

    started adding external_sort operator

---
+++ src/operators/external_sort_operator.c
@@ -0,0 +1,284 @@
+#include<rhendb/query_plan_interface.h>
+
+#include<rhendb/transaction.h>
+
+#include<rhendb/function_compare.h>
+
+/*
+	TEMPLATE FOR INTERMEDIATE OPERATORS (sorting(ordering), joins(hash_joins), aggregations(groupby->aggregates))
+*/
+
+#define MAX_LEVELS 128
+
+typedef struct input_values input_values;
+struct input_values
+{
+	operator* input_operator;
+
+	// extracted from input_operator
+	const tuple_def* record_def;
+	uint32_t key_element_count;
+	const positional_accessor* key_element_ids;
+	const compare_direction* key_compare_direction;
+
+	uint64_t minimum_run_size;
+	uint32_t N_way_sort;
+
+	pthread_mutex_t runs_lock;
+
+	int flag_no_new_un_sorted_runs;
+
+	singlylist un_sorted_runs;
+	uint64_t un_sorted_runs_count;
+
+	singlylist sorted_runs[MAX_LEVELS];
+	uint64_t sorted_runs_count[MAX_LEVELS];
+	uint64_t total_sorted_runs_count;
+};
+
+static int compare_tuples(const void* o_vp, const void* tuple1, const void* tuple2)
+{
+	const operator* o = o_vp;
+
+	int abort_error = 0;
+	int result = compare_datum2_rhendb(const datum* uval1, const datum* uval2, const data_type_info* dti, engine, NULL, &abort_error);
+
+	return result;
+}
+
+static int compare_interim_tuple_stores(const void* o_vp, const void* its1_vp, const void* its2_vp)
+{
+	const operator* o = o_vp;
+
+	const interim_tuple_store* its1_p = its1_vp;
+	const interim_tuple_store* its2_p = its2_vp;
+
+	return compare_tuples(o, its1_p->embed_regions[0].tuple, its2_p->embed_regions[0].tuple);
+}
+
+static void sort_job(operator* o, void* _param)
+{
+	input_values* inputs = o->inputs;
+
+	pthread_mutex_lock(&(inputs->runs_lock));
+	interim_tuple_store* its_p = (interim_tuple_store*) get_head_of_singlylist(&(inputs->un_sorted_runs));
+	if(its_p != NULL)
+	{
+		remove_head_from_singlylist(&(inputs->un_sorted_runs));
+		inputs->un_sorted_runs_count--;
+	}
+	pthread_mutex_unlock(&(inputs->runs_lock));
+
+	if(its_p == NULL)
+		return;
+
+	// TODO : sort its_p
+
+	pthread_mutex_lock(&(inputs->runs_lock));
+	insert_tail_in_singlylist(&(inputs->sorted_runs[0]), its_p);
+	inputs->sorted_runs_count[0]++;
+	inputs->total_sorted_runs_count++;
+	pthread_mutex_unlock(&(inputs->runs_lock));
+}
+
+static void merge_job(operator* o, void* _param)
+{
+	input_values* inputs = o->inputs;
+
+	int level = (intptr_t)(_param);
+
+	pheap mergeable_open_runs;
+	initialize_pheap(&mergeable_open_runs, MIN_HEAP, LEFTIST, &contexted_comparator(o, compare_interim_tuple_stores), offsetof(interim_tuple_store, embed_node_php));
+
+	{
+		singlylist mergeable_runs;
+		initialize_singlylist(&mergeable_runs, offsetof(interim_tuple_store, embed_node_sl));
+		uint32_t mergeable_runs_count = 0;
+
+		pthread_mutex_lock(&(inputs->runs_lock));
+		while(mergeable_runs_count < inputs->N_way_sort && !is_empty_singlylist(&(inputs->sorted_runs[level])))
+		{
+			interim_tuple_store* its_p = (interim_tuple_store*) get_head_of_singlylist(&(inputs->sorted_runs[level]));
+			if(its_p != NULL)
+			{
+				remove_head_from_singlylist(&(inputs->sorted_runs[level]));
+				insert_tail_in_singlylist(&mergeable_runs, its_p);
+				inputs->sorted_runs_count[level]--;
+				inputs->total_sorted_runs_count--;
+				mergeable_runs_count++;
+			}
+			else
+				break;
+		}
+		pthread_mutex_unlock(&(inputs->runs_lock));
+
+		if(mergeable_runs_count == 0)
+			return;
+		else if(mergeable_runs_count == 1)
+		{
+			pthread_mutex_lock(&(inputs->runs_lock));
+			insert_all_at_tail_in_singlylist(&(inputs->sorted_runs[level+1]), &mergeable_runs);
+			inputs->sorted_runs_count[level+1] += mergeable_runs_count;
+			inputs->total_sorted_runs_count += mergeable_runs_count;
+			pthread_mutex_unlock(&(inputs->runs_lock));
+			return;
+		}
+		else
+		{
+			while(!is_empty_singlylist(&mergeable_runs))
+			{
+				interim_tuple_store* its_p = (interim_tuple_store*) get_head_of_singlylist(&mergeable_runs);
+				remove_head_from_singlylist(&mergeable_runs);
+				if(!mmap_for_reading_tuple(its_p, &(its_p->embed_regions[0]), 0, &(inputs->record_def->size_def), inputs->minimum_run_size))
+					delete_interim_tuple_store(its_p);
+				else
+					push_to_pheap(&mergeable_open_runs, its_p);
+			}
+		}
+	}
+
+	interim_tuple_store* output_its_p = get_new_interim_tuple_store(".");
+
+	// merge all into output_its_p
+	while(!is_empty_pheap(&mergeable_open_runs))
+	{
+		interim_tuple_store* its_p = (interim_tuple_store*) get_top_of_pheap(&mergeable_open_runs);
+
+		// merge the top tuple of its_p into output_its_p
+		{
+			uint32_t tuple_size = get_tuple_size_using_tuple_size_def(&(inputs->record_def->size_def), its_p->embed_regions[0].tuple);
+			mmap_for_writing_tuple(output_its_p, &(output_its_p->embed_regions[0]), &(inputs->record_def->size_def), tuple_size, inputs->minimum_run_size);
+			memory_move(output_its_p->embed_regions[0].tuple, its_p->embed_regions[0].tuple, tuple_size);
+			finalize_written_tuple(output_its_p, &(output_its_p->embed_regions[0]));
+		}
+
+		// go next on its_p, and insert it back into mergeable_open_runs
+		{
+			uint64_t next_tuple_offset = next_tuple_offset_for_interim_tuple_region(&(its_p->embed_regions[0]));
+			if(!mmap_for_reading_tuple(its_p, &(its_p->embed_regions[0]), next_tuple_offset, &(inputs->record_def->size_def), inputs->minimum_run_size))
+			{
+				unmap_for_interim_tuple_region(&(its_p->embed_regions[0]));
+				delete_interim_tuple_store(its_p);
+			}
+			else
+				push_to_pheap(&mergeable_open_runs, its_p);
+		}
+	}
+
+	pthread_mutex_lock(&(inputs->runs_lock));
+	insert_tail_in_singlylist(&(inputs->sorted_runs[level+1]), output_its_p);
+	inputs->sorted_runs_count[level+1]++;
+	inputs->total_sorted_runs_count++;
+	pthread_mutex_unlock(&(inputs->runs_lock));
+}
+
+static void execute(operator* o)
+{
+	input_values* inputs = o->inputs;
+
+	dstring kill_reason = get_dstring_pointing_to_literal_cstring("completed_and_killed");
+
+	while(1)
+	{
+		int no_more_data = 0;
+		interim_tuple_store* its_p = consume_from_operator(inputs->input_operator, inputs->minimum_run_size, &no_more_data);
+		if(no_more_data)
+		{
+			if(is_killed_operator(inputs->input_operator))
+			{
+				pthread_mutex_lock(&(inputs->runs_lock));
+				inputs->flag_no_new_un_sorted_runs = 1;
+				pthread_mutex_unlock(&(inputs->runs_lock));
+				break;
+			}
+			else
+				break;
+		}
+		if(can_not_proceed_for_execution_operator(o))
+		{
+			mark_operator_self_killed(o, kill_reason); return ;
+		}
+
+		if(its_p != NULL)
+		{
+			pthread_mutex_lock(&(inputs->runs_lock));
+			insert_tail_in_singlylist(&(inputs->un_sorted_runs), its_p);
+			inputs->un_sorted_runs_count++;
+			pthread_mutex_unlock(&(inputs->runs_lock));
+
+			run_concurrent_job_for_operator(o, NULL, sort_job);
+		}
+		else
+			break;
+	}
+
+	return ;
+}
+
+static void free_resources(operator* o)
+{
+	input_values* inputs = o->inputs;
+
+	pthread_mutex_lock(&(inputs->runs_lock));
+
+	for(int i = 0; i < MAX_LEVELS; i++)
+	{
+		while(!is_empty_singlylist(&(inputs->sorted_runs[i])))
+		{
+			interim_tuple_store* its_p = (interim_tuple_store*) get_head_of_singlylist(&(inputs->sorted_runs[i]));
+			remove_head_from_singlylist(&(inputs->sorted_runs[i]));
+			delete_interim_tuple_store(its_p);
+			inputs->sorted_runs_count[i]--;
+			inputs->total_sorted_runs_count--;
+		}
+	}
+
+	while(!is_empty_singlylist(&(inputs->un_sorted_runs)))
+	{
+		interim_tuple_store* its_p = (interim_tuple_store*) get_head_of_singlylist(&(inputs->un_sorted_runs));
+		remove_head_from_singlylist(&(inputs->un_sorted_runs));
+		delete_interim_tuple_store(its_p);
+		inputs->un_sorted_runs_count--;
+	}
+
+	pthread_mutex_unlock(&(inputs->runs_lock));
+}
+
+#include<rhendb/tuple_transformers.h>
+
+void setup_external_sort_operator(operator* o, operator* input_operator, uint32_t key_element_count, const positional_accessor* key_element_ids, const compare_direction* key_compare_direction, uint64_t minimum_run_size, uint32_t N_way_sort)
+{
+	// force the input operator to produce tuples one by one
+	append_tuple_transformer(&(input_operator->output_tuple_transformers), get_new_identity_tuple_transformer(get_tuple_def_for_tuples_to_be_consumed_from(input_operator)));
+
+	o->execute = execute;
+	o->operator_release_latches_and_store_context = OPERATOR_RELEASE_LATCH_NO_OP_FUNCTION;
+	o->free_resources = free_resources;
+
+	// it is an identity operator, produces the same thing as it consumes
+	init_tuple_transformers(&(o->output_tuple_transformers), get_tuple_def_for_tuples_to_be_consumed_from(input_operator));
+
+	o->inputs = malloc(sizeof(input_values));
+	input_values* inputs = o->inputs;
+	*inputs = (input_values){
+		.input_operator = input_operator,
+		.record_def = get_tuple_def_for_tuples_to_be_consumed_from(input_operator),
+		.key_element_count = key_element_count,
+		.key_element_ids = key_element_ids,
+		.key_compare_direction = key_compare_direction,
+		.minimum_run_size = minimum_run_size,
+		.N_way_sort = N_way_sort,
+		.runs_lock = PTHREAD_MUTEX_INITIALIZER,
+		.flag_no_new_un_sorted_runs = 0,
+		.un_sorted_runs_count = 0,
+		.sorted_runs_count = {},
+		.total_sorted_runs_count = 0,
+	};
+
+	for(int i = 0; i < MAX_LEVELS; i++)
+		initialize_singlylist(&(inputs->sorted_runs[i]), offsetof(interim_tuple_store, embed_node_sl));
+	initialize_singlylist(&(inputs->un_sorted_runs), offsetof(interim_tuple_store, embed_node_sl));
+
+	input_operator->consumer_operator = o;
+	input_operator->consumer_trigger_on_bytes_accumulated = minimum_run_size;
+}
