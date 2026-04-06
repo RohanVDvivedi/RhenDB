5eaa19f47cd515a75fcbbd2fccbc1a51b513680d
RohanVDvivedi <rohandvivedi@gmail.com>
2026-04-06 22:10:55 +0530

    added declarations for the rash table

---
+++ inc/rhendb/rash_table.h
@@ -0,0 +1,105 @@
+#ifndef RASH_TABLE_H
+#define RASH_TABLE_H
+
+#include<rhendb/rage_engine.h>
+
+/*
+	rash_table is an in-memory hash table built with the sole purpose of execution of queries quickly
+
+	it in reality stores the following, tuple
+
+	hash_value(uint64_t actual_key), key_size(uint64_t for_compare_only), key(tuple_list, with prefix of 200 bytes for_compare_only), value(blob with prefix of 140 bytes)
+
+	it will alays be ensured that the records ill alyas store unique values for (hash_value, key_size, key)
+*/
+
+#define PREFIX_BYTES_FOR_KEY           200
+#define PREFIX_BYTES_FOR_VALUE         140
+
+#define EXTENDED_TYPE_MAX_SIZE_FOR_KEY    ((4 + (4 + 8)) + (4 + PREFIX_BYTES_FOR_KEY))
+#define EXTENDED_TYPE_MAX_SIZE_FOR_VALUE  ((4 + (4 + 8)) + (4 + PREFIX_BYTES_FOR_VALUE))
+
+#define INLINE_RASH_RECORD_SIZE        ((4 + (8 + 8 + 4 + 4)) + EXTENDED_TYPE_MAX_SIZE_FOR_KEY + EXTENDED_TYPE_MAX_SIZE_FOR_VALUE)
+
+fail_build_on(INLINE_RASH_RECORD_SIZE > 1024);
+
+#define MIN_LOAD_FACTOR_IN_BYTES   7 // shrink if crossed
+#define MAX_LOAD_FACTOR_IN_BYTES  20 // expand if crossed
+
+typedef struct rash_table_handle rash_table_handle;
+struct rash_table_handle
+{
+	uint64_t root_page_id;
+
+	uint64_t element_count; // total number of elements
+	uint64_t bucket_count; // total number of buckets
+
+	uint64_t total_inline_size; // total number of inline bytes used
+
+	/*
+		expand if (total_inline_size / (bucket_count * PAGE_SIZE)) > MAX_LOAD_FACTOR_IN_BYTES
+		shrink if (total_inline_size / (bucket_count * PAGE_SIZE)) < MIN_LOAD_FACTOR_IN_BYTES
+	*/
+
+	tuple_defs* key_tuple_defs; // array of tuple_defs, built with the handle
+	uint32_t key_element_count;
+
+	// to store it's own data/tuples
+	rage_engine* volatile_engine;
+
+	// to read contents of extended types, to hash and compare them
+	rage_engine* ex_engine;
+};
+
+rash_table_handle get_new_rash_table1(const data_type_info** key_dtis, uint32_t key_element_count, rage_engine* volatile_engine, rage_engine* ex_engine);
+rash_table_handle get_new_rash_table2(const tuple_def* record_def, const positional_accessor* key_element_ids, uint32_t key_element_count, rage_engine* volatile_engine, rage_engine* ex_engine);
+
+void destroy_rash_table(rash_table_handle* rth_p);
+
+typedef struct rash_table_key rash_table_key;
+struct rash_table_key
+{
+	const void* record;
+	const tuple_def* record_def;
+	const positional_accessor* key_element_ids;
+	uint32_t key_element_count;
+
+	int is_hash_value_valid;
+	uint64_t hash_value;
+};
+
+void initialize_rash_table_key(rash_table_key* rkey_p, const void* record, const tuple_def* record_def, const positional_accessor* key_element_ids, uint3_t key_element_count);
+
+// after this call is_hash_value_valid will be set to 1, for this rkey_p
+uint64_t get_hash_value_for_rash_table_key(rash_table_key* rkey_p);
+
+typedef struct rash_table_iterator rash_table_iterator;
+struct rash_table_iterator
+{
+	rash_table_handle* rth_p;
+
+	hash_table_iterator* hti_p;
+
+	// this rkey_p may be NULL, if you are iterating over all the keys in the rash_table
+	rash_table_key* rkey_p;
+};
+
+rash_table_iterator* find_all_in_rash_table(rash_table_handle* rth_p);
+
+rash_table_iterator find_equals_in_rash_table(rash_table_handle* rth_p, const rash_table_key* rkey_p);
+
+int exists_in_rash_table_iterator(const rash_table_iterator* rti_p);
+
+int remove_from_rash_table_iterator(rash_table_iterator* rti_p);
+
+binary_read_iterator* read_value_in_rash_table_iterator(rash_table_iterator* rti_p);
+
+binary_write_iterator* open_for_writing_value_in_rash_table_iterator(rash_table_iterator* rti_p);
+void close_and_write_value_in_hash_table_iterator(rash_table_iterator* rti_p, binary_write_iterator* bwi_p);
+
+// fails if rash_table_key is found in the iterator
+void next_in_rash_table_iterator(rash_table_iterator* rti_p);
+
+void delete_rash_table_iterator(rash_table_iterator* rti_p);
+
+#endif
