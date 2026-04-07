692183148d036de40b6f49f2f1d10f23ca58004a
RohanVDvivedi <rohandvivedi@gmail.com>
2026-04-07 23:23:26 +0530

    more flags added for compilation

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,6 +1,6 @@
-gcc ./test_mvcc.c -o test_mvcc.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
-gcc ./test_ttbl.c -o test_ttbl.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
-gcc ./test_lckmgr.c -o test_lckmgr.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
-gcc ./test_its.c -o test_its.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
-gcc ./test_dops.c -o test_dops.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
-gcc -O3 -flto ./test_sorter.c -o test_sorter.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
+gcc -Wall -O3 -flto ./test_mvcc.c -o test_mvcc.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
+gcc -Wall -O3 -flto ./test_ttbl.c -o test_ttbl.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
+gcc -Wall -O3 -flto ./test_lckmgr.c -o test_lckmgr.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
+gcc -Wall -O3 -flto ./test_its.c -o test_its.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
+gcc -Wall -O3 -flto ./test_dops.c -o test_dops.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
+gcc -Wall -O3 -flto ./test_sorter.c -o test_sorter.out -lrhendb -lmintxengine -ltuplelargetypes -lvolatilepagestore -ltupleindexer -ltuplestore -lbufferpool -lwale -lblockio -llockking -lboompar -lcutlery -lz
