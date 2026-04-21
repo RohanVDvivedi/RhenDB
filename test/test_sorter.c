6769585848ffbc94ceed781962c2ea6bdd2202ff
RohanVDvivedi <rohandvivedi@gmail.com>
2026-04-21 23:03:07 +0530

    --

--- test/test_sorter.c
+++ test/test_sorter.c
@@ -19,7 +19,7 @@
 
 #define TESTCASE_SIZE 1000000
 
-//#define PRINT_DATA 1
+#define PRINT_DATA 0
 
 #define SMALLEST_RUN_SIZE              (1 * 1024 * 1024)
 #define PARALLEL_SORTING_JOBS_COUNT    8
