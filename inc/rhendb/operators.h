9b80d1e7924bd6683c9276e544858e4438ebe9ca
RohanVDvivedi <rohandvivedi@gmail.com>
2026-04-21 21:21:41 +0530

    added necessary ehaders

--- inc/rhendb/operators.h
+++ inc/rhendb/operators.h
@@ -9,9 +9,11 @@ void setup_identity_operator(operator* o, operator* input_operator);
 
 void setup_printf_operator(operator* o, operator* input_operator, int do_print);
 
+void setup_result_match_operator(operator* o, operator* input_operators[2]);
+
 void setup_external_sort_operator(operator* o, operator* input_operator, uint32_t key_element_count, const positional_accessor* key_element_ids, const compare_direction* key_compare_direction, uint64_t minimum_run_size, uint32_t N_way_sort, uint32_t max_concurrent_jobs_count);
 
-void setup_result_match_operator(operator* o, operator* input_operators[2]);
+#include<cutlery/stream.h>
 
 void setup_stream_input_operator(operator* o, stream* in_strm, const tuple_def* input_tuple_def);
 
