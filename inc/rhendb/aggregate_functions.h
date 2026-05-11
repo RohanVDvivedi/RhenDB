ad23dc8888328bd7e8af91c7fd037571f0bec536
RohanVDvivedi <rohandvivedi@gmail.com>
2026-05-11 23:03:32 +0530

    simple aggregate function for counting tuples added

---
+++ inc/rhendb/aggregate_functions.h
@@ -0,0 +1,63 @@
+#ifndef AGGREGATE_FUNCTIONS_H
+#define AGGREGATE_FUNCTIONS_H
+
+#include<tuplestore/data_type_info.h>
+#include<tuplestore/datum.h>
+
+#define AGGREGATE_FUNCTION_PARAM_TYPE_INFO_HOLDER_CAPACITY 4
+
+typedef struct aggregate_function aggregate_function;
+struct aggregate_function
+{
+	const void* context_p;
+
+	// returns 0 on failure
+	int (*process_input)(const aggregate_function* af_p, void** state_p, const datum* inputs[]);
+
+	// destroys state object, must be NO-OP if NULL
+	void (*destroy_state)(const aggregate_function* af_p, void* state);
+
+	// returns 0 on failure
+	int (*produce_output)(const aggregate_function* af_p, datum* output, const void* state);
+
+	// use this function to destroy the output produced, NO-OP on NULL_DATUM
+	void (*destroy_output)(const aggregate_function* af_p, datum output);
+
+	// destroys this object
+	void (*destroy_aggregate_function)(aggregate_function* af_p);
+
+	// single value storing the type_info for the aggregate_function's output
+	const data_type_info* output_type_info;
+
+	// number of elements in input_type_infos[]
+	uint32_t input_type_infos_count;
+
+	// type_infos for the elements that go in as input to the process_input() function
+	const data_type_info* input_type_infos[];
+};
+
+#define size_of_aggregate_function(input_params_count) (sizeof(aggregate_function) + ((input_params_count) * sizeof(data_type_info*)))
+
+/*
+	usage guide (not exactly C)
+
+	void* state = NULLl;
+	datum ouput = (*NULL_DATUM);
+
+	af_p->process_input(af_p, &state, inputs_array_0[]);
+	af_p->process_input(af_p, &state, inputs_array_1[]);
+	af_p->process_input(af_p, &state, inputs_array_2[]);
+	af_p->process_input(af_p, &state, inputs_array_3[]);
+	.
+	.
+	.
+
+	// then when all input tuples are finished
+
+	af_p->produce_output(af_p, &output, state);
+	af_p->destroy_state(af_p, state);
+	// give output to the expecting user
+	af_p->destroy_output(af_p, output);
+*/
+
+#endif
---
+++ src/aggregate_functions/count_aggregate_function.c
@@ -0,0 +1,66 @@
+#include<rhendb/aggregate_functions.h>
+
+#include<tuplestore/tuple_def.h>
+#include<tuplestore/tuple.h>
+
+#include<stdlib.h>
+
+static int process_input(const aggregate_function* af_p, void** state_p, const datum* inputs[])
+{
+	if((*state_p) == NULL)
+		(*state_p) = calloc(sizeof(uint64_t), 1);
+
+	if(!is_datum_NULL(inputs[0]))
+		(**((uint64_t**)state_p))++;
+
+	return 1;
+}
+
+static void destroy_state(const aggregate_function* af_p, void* state)
+{
+	free(state);
+}
+
+static int produce_output(const aggregate_function* af_p, datum* output, const void* state)
+{
+	(*output) = (datum){.uint_value = 0};
+
+	if(state != NULL)
+		output->uint_value = (*((const uint64_t*)state));
+
+	return 1;
+}
+
+static void destroy_output(const aggregate_function* af_p, datum output)
+{
+	// it is storing just a .uint_value
+}
+
+static void destroy_aggregate_function(aggregate_function* af_p)
+{
+	free(af_p);
+}
+
+aggregate_function* get_count_aggregate_function(const data_type_info* input_type_info)
+{
+	aggregate_function* af_p = malloc(size_of_aggregate_function(1));
+
+	af_p->context_p = NULL;
+
+	af_p->process_input = process_input;
+
+	af_p->destroy_state = destroy_state;
+
+	af_p->produce_output = produce_output;
+
+	af_p->destroy_output = destroy_output;
+
+	af_p->destroy_aggregate_function = destroy_aggregate_function;
+
+	af_p->output_type_info = UINT_NON_NULLABLE[8];
+
+	af_p->input_type_infos_count = 1;
+	af_p->input_type_infos[0] = input_type_info;
+
+	return af_p;
+}
