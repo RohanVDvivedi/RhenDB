bf399ac406b1f8a6ba40aca809c0d1043458e7e9
RohanVDvivedi <rohandvivedi@gmail.com>
2026-05-29 18:59:45 +0530

    result match operator now uses the new api

--- src/operators/result_match_operator.c
+++ src/operators/result_match_operator.c
@@ -28,8 +28,6 @@ static void execute(operator* o)
 {
 	input_values* inputs = o->inputs;
 
-	dstring kill_reason = get_dstring_pointing_to_literal_cstring("completed_and_killed");
-
 	while(1)
 	{
 		// get all next tuples
@@ -48,44 +46,35 @@ static void execute(operator* o)
 		// we reach here only if they are all non-NULL or have no more data
 		if(inputs->no_more_data[0] != inputs->no_more_data[1])
 		{
-			destroy_consumption_iterator(inputs->input_iterators[0]); inputs->input_iterators[0] = NULL;
-			destroy_consumption_iterator(inputs->input_iterators[1]); inputs->input_iterators[1] = NULL;
-
 			if(inputs->no_more_data[0])
 			{
-				kill_reason = get_dstring_pointing_to_literal_cstring("resuts_not_match_more_data_in_input_0");
-				kill_signal_for_self_operator(o, kill_reason); return ;
+				kill_signal_for_self_operator(o, get_dstring_pointing_to_literal_cstring("resuts_not_match_more_data_in_input_0"));
+				return ;
 			}
 			else
 			{
-				kill_reason = get_dstring_pointing_to_literal_cstring("resuts_not_match_more_data_in_input_1");
-				kill_signal_for_self_operator(o, kill_reason); return ;
+				kill_signal_for_self_operator(o, get_dstring_pointing_to_literal_cstring("resuts_not_match_more_data_in_input_1"));
+				return ;
 			}
 		}
 
 		if(inputs->no_more_data[0] == 1)
 		{
-			destroy_consumption_iterator(inputs->input_iterators[0]); inputs->input_iterators[0] = NULL;
-			destroy_consumption_iterator(inputs->input_iterators[1]); inputs->input_iterators[1] = NULL;
-
-			kill_reason = get_dstring_pointing_to_literal_cstring("resuts_match_successfully");
 			printf("matched %"PRIu64" tuples\n", inputs->tuple_processed);
-			kill_signal_for_self_operator(o, kill_reason); return ;
+			kill_signal_for_self_operator(o, get_dstring_pointing_to_literal_cstring("resuts_match_successfully"));
+			return ;
 		}
 
 		int compare = compare_tuples_rhendb(inputs->tuples[0], inputs->input_defs[0], NULL, inputs->tuples[1], inputs->input_defs[1], NULL, NULL, inputs->element_count, &(o->self_query_plan->curr_tx->db->persistent_acid_rage_engine));
 		inputs->tuple_processed++;
 		if(compare != 0)
 		{
-			destroy_consumption_iterator(inputs->input_iterators[0]); inputs->input_iterators[0] = NULL;
-			destroy_consumption_iterator(inputs->input_iterators[1]); inputs->input_iterators[1] = NULL;
-
 			printf("result_match_operator says outputs do not match for %"PRIu64"-th tuple:\n", inputs->tuple_processed);
 			print_tuple(inputs->tuples[0], inputs->input_defs[0]);
 			print_tuple(inputs->tuples[1], inputs->input_defs[1]);
 			printf("\n\n");
-			kill_reason = get_dstring_pointing_to_literal_cstring("resuts_match_FAILED");
-			kill_signal_for_self_operator(o, kill_reason); return ;
+			kill_signal_for_self_operator(o, get_dstring_pointing_to_literal_cstring("resuts_match_FAILED"));
+			return ;
 		}
 
 		for(int i = 0; i < 2; i++)
@@ -93,6 +82,22 @@ static void execute(operator* o)
 	}
 
 	return ;
+}
+
+static void clean_up_resources(operator* o)
+{
+	input_values* inputs = o->inputs;
+
+	if(inputs->input_iterators[0] != NULL)
+	{
+		destroy_consumption_iterator(inputs->input_iterators[0]);
+		inputs->input_iterators[0] = NULL;
+	}
+	if(inputs->input_iterators[1] != NULL)
+	{
+		destroy_consumption_iterator(inputs->input_iterators[1]);
+		inputs->input_iterators[1] = NULL;
+	}
 }
 
 operator_resource_counter setup_result_match_operator(operator* o, operator* input_operators[2])
@@ -135,6 +140,7 @@ operator_resource_counter setup_result_match_operator(operator* o, operator* inp
 
 	o->execute = execute;
 	o->operator_release_latches_and_store_context = OPERATOR_RELEASE_LATCH_NO_OP_FUNCTION;
+	o->clean_up_resources = clean_up_resources;
 	o->free_resources = OPERATOR_FREE_RESOURCE_NO_OP_FUNCTION;
 
 	o->inputs = malloc(sizeof(input_values));
