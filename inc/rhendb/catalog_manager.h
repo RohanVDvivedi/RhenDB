6178bc3d01cd9fdde81d264755e9af185077ed40
RohanVDvivedi <rohandvivedi@gmail.com>
2026-06-11 22:52:56 +0530

    --

--- inc/rhendb/catalog_manager.h
+++ inc/rhendb/catalog_manager.h
@@ -13,7 +13,7 @@ enum catalog_object_type
 {
 	RHENDB_TYPE = 0,
 	RHENDB_ATTRIBUTE = 1,
-	RHENDB_RELATION = 2,
+	RHENDB_TABLE = 2,
 	RHENDB_INDEX = 3,
 	RHENDB_FUNCTION = 4,
 };
@@ -33,8 +33,8 @@ struct catalog_manager
 	heap_table_tuple_defs db_attributes_tuple_defs;
 	uint64_t db_attributes_root_page_id;
 
-	heap_table_tuple_defs db_relations_tuple_defs;
-	uint64_t db_relations_root_page_id;
+	heap_table_tuple_defs db_tables_tuple_defs;
+	uint64_t db_tables_root_page_id;
 
 	heap_table_tuple_defs db_indices_tuple_defs;
 	uint64_t db_indices_root_page_id;
@@ -54,8 +54,32 @@ struct catalog_manager
 
 	// rage_engine to be used with catalog manager
 	rage_engine* catmgr_engine;
+
+	// this where new ids come from
+	pthread_mutex_t global_id_lock;
+	uint64_t global_id;
 };
 
 void initialize_catalog_manager(catalog_manager* catmgr_p, uint64_t* root_page_id, data_type_info* mvcc_hdr_dti_p, rage_engine* catmgr_engine);
+
+// returns id of created table
+uint64_t create_table(char* name);
+
+// returns part_id
+uint64_t alter_table(uint64_t id);
+
+void drop_table(uint64_t id);
+
+// returns id of created index
+uint64_t create_index(char* name);
+void drop_index(uint64_t id);
+
+// returns id of created type
+uint64_t create_type(char* name);
+void drop_type(uint64_t id);
+
+// returns id of created function
+uint64_t create_function(char* name);
+void drop_function(uint64_t id);
 
 #endif
