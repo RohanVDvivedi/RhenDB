a5d000922b4a16d97f7d9d1ca361818cae73a7fa
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-04 22:18:46 +0530

    simple projection operator now formalized to hold enough max bytes as it needs, minor fix, allows duplicate columns to be projected, row id and row num now need 24 extra bytes (size + uint64_t counter/identifier, 8 byte offset for a variable length tuple), made intermediate tuple size a global macro

---
+++ inc/rhendb/max_intermediate_tuple_size.h
@@ -0,0 +1,6 @@
+#ifndef MAX_INTERMEDIATE_TUPLE_SIZE_H
+#define MAX_INTERMEDIATE_TUPLE_SIZE_H
+
+#define MAX_INTERMEDIATE_TUPLE_SIZE 3.9e9 // set to 3.9 billion (4.2 is max value for this) bytes
+
+#endif
