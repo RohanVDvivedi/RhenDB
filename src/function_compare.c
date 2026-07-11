5118a52d22d26edff928f0272370f1e10192cfd0
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-11 17:59:51 +0530

    can compare datum revamped to compare nested tuples that look identical

--- src/function_compare.c
+++ src/function_compare.c
@@ -8,7 +8,7 @@
 
 int can_compare_datum_rhendb(const data_type_info* dti1, const data_type_info* dti2)
 {
-	if(are_identical_type_info(dti1, dti2))
+	if(dti1 == dti2)
 		return 1;
 	else if(!is_container_type_info(dti1) && !is_container_type_info(dti2)) // non container types, primitive numbers: bit_field, uint, int, large_uint, large_int, float
 		return can_compare_datum(dti1, dti2);
@@ -20,6 +20,15 @@ int can_compare_datum_rhendb(const data_type_info* dti1, const data_type_info* d
 		return 0;
 	else if((dti1->type == STRING || dti1->type == BINARY || dti1->type == ARRAY) && (dti2->type == STRING || dti2->type == BINARY || dti2->type == ARRAY)) // STRING, BINARY and ARRAY are internally comparable, if their containee types are comparable
 		return can_compare_datum_rhendb(dti1->containee, dti2->containee); // recursive call, so 2 inline arrays of extended-text types are comparable
+	else if(dti1->type == TUPLE && dti2->type == TUPLE)
+	{
+		if(dti1->element_count != dti2->element_count)
+			return 0;
+		for(uint32_t i = 0; i < dti1->element_count; i++)
+			if(!can_compare_datum_rhendb(get_data_type_info_for_containee_of_container_without_data(dti1, i), get_data_type_info_for_containee_of_container_without_data(dti2, i)))
+				return 0;
+		return 1;
+	}
 	else // else it is not identical inline tuple, and they can not be compared directly
 		return 0;
 
