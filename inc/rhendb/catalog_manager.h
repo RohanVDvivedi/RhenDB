d5c3a2bfab106a897b3c70c388f1e83a88e3e5f9
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-14 22:03:51 +0530

    need catalog manager on every function

--- inc/rhendb/catalog_manager.h
+++ inc/rhendb/catalog_manager.h
@@ -21,7 +21,7 @@ enum catalog_object_type
 typedef struct catalog_manager catalog_manager;
 struct catalog_manager
 {
-	// this page_table stores the 
+	// this page_table stores the all the root_page_ids of the static schema-ed catalog tables
 	page_table_tuple_defs catalog_root_tuple_defs;
 	uint64_t catalog_root_page_id;
 
@@ -44,11 +44,11 @@ struct catalog_manager
 
 	// ---------------- INDICES ON SCHEMA TABLE
 
-	// bplus_tree index object_type, id, part_id -> row_id
+	// bplus_tree index object_type, id, partition_id -> row_id (included in the key)
 	bplus_tree_tuple_defs idx_id_part_tuple_defs;
 	uint64_t idx_id_part_root_page_id;
 
-	// bplus_tree index object_type, name -> row_id
+	// bplus_tree index object_type, name -> row_id (included in the key)
 	bplus_tree_tuple_defs idx_name_tuple_defs;
 	uint64_t idx_name_root_page_id;
 
@@ -60,26 +60,30 @@ struct catalog_manager
 	uint64_t global_id;
 };
 
+// here the root_page_id is an in-out parameter, pass it as NULL_PAGE_ID to create a new transaction table, or an existing one to open that particular transaction_table
 void initialize_catalog_manager(catalog_manager* catmgr_p, uint64_t* root_page_id, data_type_info* mvcc_hdr_dti_p, rage_engine* catmgr_engine);
 
 // returns id of created table
-uint64_t create_table(char* name);
+uint64_t create_table(catalog_manager* catmgr_p, char* name);
 
-// returns part_id
-uint64_t alter_table(uint64_t id);
+// returns new partition_id, and also creates new partitions of existing indices of the table
+uint64_t alter_table(catalog_manager* catmgr_p, uint64_t id);
 
-void drop_table(uint64_t id);
+// drops all it's partitions, and all indices and their partitions
+void drop_table(catalog_manager* catmgr_p, uint64_t id);
 
-// returns id of created index
-uint64_t create_index(char* name);
-void drop_index(uint64_t id);
+// returns id of created index, and creates the same index for all the partitions of this table
+uint64_t create_index(catalog_manager* catmgr_p, char* name, uint64_t table_id);
+
+// drops index with this id and all it's partitions
+void drop_index(catalog_manager* catmgr_p, uint64_t id);
 
 // returns id of created type
-uint64_t create_type(char* name);
-void drop_type(uint64_t id);
+uint64_t create_type(catalog_manager* catmgr_p, char* name);
+void drop_type(catalog_manager* catmgr_p, uint64_t id);
 
 // returns id of created function
-uint64_t create_function(char* name);
-void drop_function(uint64_t id);
+uint64_t create_function(catalog_manager* catmgr_p, char* name);
+void drop_function(catalog_manager* catmgr_p, uint64_t id);
 
 #endif
