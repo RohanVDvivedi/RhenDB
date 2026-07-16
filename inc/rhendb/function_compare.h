a2b8b0793b8a62c31e8155692be36ca8fd8be72b
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-17 00:37:45 +0530

    compare and hash functions will now work with transaction pointer and not rage engine pointer

--- inc/rhendb/function_compare.h
+++ inc/rhendb/function_compare.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_COMPARE_H
 #define FUNCTION_COMPARE_H
 
-#include<rhendb/rage_engine.h>
+#include<rhendb/transaction.h>
 
 #include<tuplestore/datum.h>
 
@@ -17,15 +17,15 @@ int can_compare_datum_rhendb(const data_type_info* dti1, const data_type_info* d
 
 // transaction_id is passed as NULL, to read extended types as a separate read-only transaction
 
-int compare_datum_rhendb(const datum* uval1, const data_type_info* dti1, const datum* uval2, const data_type_info* dti2, rage_engine* ex_engine);
+int compare_datum_rhendb(const datum* uval1, const data_type_info* dti1, const datum* uval2, const data_type_info* dti2, transaction* tx);
 
 // this function is added to skip the checks that ensure that the datums can be compared, so that such checks can be discarded
-int compare_datum2_rhendb(const datum* uval1, const datum* uval2, const data_type_info* dti, rage_engine* ex_engine);
+int compare_datum2_rhendb(const datum* uval1, const datum* uval2, const data_type_info* dti, transaction* tx);
 
-int compare_tuples_rhendb(const void* tup1, const tuple_def* tpl_d1, const positional_accessor* element_ids1, const void* tup2, const tuple_def* tpl_d2, const positional_accessor* element_ids2, const compare_direction* cmp_dir, uint32_t element_count, rage_engine* ex_engine);
+int compare_tuples_rhendb(const void* tup1, const tuple_def* tpl_d1, const positional_accessor* element_ids1, const void* tup2, const tuple_def* tpl_d2, const positional_accessor* element_ids2, const compare_direction* cmp_dir, uint32_t element_count, transaction* tx);
 
-int compare_tuples2_rhendb(const void* tup1, const void* tup2, const tuple_def* tpl_d, const positional_accessor* element_ids, const compare_direction* cmp_dir, uint32_t element_count, rage_engine* ex_engine);
+int compare_tuples2_rhendb(const void* tup1, const void* tup2, const tuple_def* tpl_d, const positional_accessor* element_ids, const compare_direction* cmp_dir, uint32_t element_count, transaction* tx);
 
-int compare_datums3_rhendb(const datum* uvals1, const datum* uvals2, data_type_info const * const * dtis, const compare_direction* cmp_dir, uint32_t element_count, rage_engine* ex_engine);
+int compare_datums3_rhendb(const datum* uvals1, const datum* uvals2, data_type_info const * const * dtis, const compare_direction* cmp_dir, uint32_t element_count, transaction* tx);
 
 #endif
--- inc/rhendb/function_hash.h
+++ inc/rhendb/function_hash.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_HASH_H
 #define FUNCTION_HASH_H
 
-#include<rhendb/rage_engine.h>
+#include<rhendb/transaction.h>
 
 #include<tuplestore/datum.h>
 
@@ -15,8 +15,8 @@ int do_these_types_on_being_equal_hash_to_same_value(const data_type_info* dti1,
 // it only reads data from the ex_engine for the extended atributes, provision atleast 1 buffer for each one of this function calls
 // the ex_engine will/must be the min_tx_engine
 // transaction_id is passed as NULL, to read extended types as a separate read-only transaction
-uint64_t hash_datum_rhendb(const datum* uval, const data_type_info* dti, tuple_hasher* th, rage_engine* ex_engine);
+uint64_t hash_datum_rhendb(const datum* uval, const data_type_info* dti, tuple_hasher* th, transaction* tx);
 
-uint64_t hash_tuple_rhendb(const void* tup, const tuple_def* tpl_d, const positional_accessor* element_ids, tuple_hasher* th, uint32_t element_count, rage_engine* ex_engine);
+uint64_t hash_tuple_rhendb(const void* tup, const tuple_def* tpl_d, const positional_accessor* element_ids, tuple_hasher* th, uint32_t element_count, transaction* tx);
 
 #endif
