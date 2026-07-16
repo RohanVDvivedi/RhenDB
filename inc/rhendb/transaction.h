f409dc25e1e6da9e3268a8e2b98f62aded25cf75
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-17 00:01:34 +0530

    transaction object how holds temporary stores to be used by query plans

--- inc/rhendb/transaction.h
+++ inc/rhendb/transaction.h
@@ -4,25 +4,54 @@
 #include<rhendb/rhendb.h>
 #include<rhendb/mvcc_snapshot.h>
 
+#include<lockking/rwlock.h>
+
 // the transaction struct only consists of pointers to already created structs, and need to be managed by you
 // it is basically a place holder for you (the user), and noone else, the application will not be handling or initializing it for you
 
 // this struct must be 0 initiialized, before trying to use it
 
+// extended types when we are working with them, may be stored on disk blob_store using persistent_acid_rage_engine for the corresponding table
+// or in the volatile_rage_engine 
+// given below sub_type information helps us organize them and store them at the right place
+// this type information for thsi tuplelargetypes itself helps us with that
+// this is only used when the extended type is tex, numeric or blob
+
+#define PERSISTENT_EXT_SUB_TYPE    "P" // for values read from disk, i.e. from persistent_acid_rage_engine
+
+#define VOLATILE_EXT_SUB_TYPE      "V" // for temporary data generated from expressions, are stored in this type, in volatile_rage_engine
+
+typedef struct temporary_extension_store temporary_extension_store;
+struct temporary_extension_store
+{
+	uint64_t blob_store_root_page_id;
+
+	rwlock blob_store_lock;
+};
+
+// this is the number of temporary extension stores that any 1 particular transaction will maintain
+#define TEMPORARY_EXTENSION_STORE_COUNT 64
+
 typedef struct transaction transaction;
 struct transaction
 {
 	// database that this transaction belongs to
-	rhendb* db;
+	rhendb* rdb;
 
 	// mvcc snapshot, for isolation level
 	mvcc_snapshot* snapshot;
 
 	// transaction_id points to the transaction_id in the snapshot for this transaction
 	uint256* transaction_id;
+
+	// the array that holds these temporary extension blobs, access them by the hash of the prefix
+	temporary_extension_store temp_ext_stores[TEMPORARY_EXTENSION_STORE_COUNT];
 };
 
-transaction initialize_transaction(rhendb* db);
+transaction initialize_transaction(rhendb* rdb);
+
+// deletes the old temp_ext_stores and creates new blobs for them
+void reset_temp_ext_stores_in_transaction(transaction* tx);
 
 void deinitialize_transaction(transaction* tx);
 
--- src/transaction.c
+++ src/transaction.c
@@ -1,21 +1,46 @@
 #include<rhendb/transaction.h>
 
-transaction initialize_transaction(rhendb* db)
+transaction initialize_transaction(rhendb* rdb)
 {
 	transaction tx = {
-		.db = db,
+		.rdb = rdb,
 		.snapshot = NULL,
 		.transaction_id = NULL,
-		.curr_query = NULL,
 	};
+
+	for(uint32_t i = 0; i < TEMPORARY_EXTENSION_STORE_COUNT; i++)
+	{
+		const void* transaction_id = NULL;
+		int abort_error_dummy = 0;
+		tx.temp_ext_stores[i].blob_store_root_page_id = get_new_blob_store(&(rdb->volatile_rage_engine.bstd), rdb->volatile_rage_engine.pam_p, rdb->volatile_rage_engine.pmm_p, transaction_id, &abort_error_dummy);
+		initialize_rwlock(&(tx.temp_ext_stores[i].blob_store_lock), NULL);
+	}
 
 	return tx;
 }
 
+void reset_temp_ext_stores_in_transaction(transaction* tx)
+{
+	for(uint32_t i = 0; i < TEMPORARY_EXTENSION_STORE_COUNT; i++)
+	{
+		const void* transaction_id = NULL;
+		int abort_error_dummy = 0;
+		destroy_blob_store(tx->temp_ext_stores[i].blob_store_root_page_id, &(tx->rdb->volatile_rage_engine.bstd), tx->rdb->volatile_rage_engine.pam_p, transaction_id, &abort_error_dummy);
+		tx->temp_ext_stores[i].blob_store_root_page_id = get_new_blob_store(&(tx->rdb->volatile_rage_engine.bstd), tx->rdb->volatile_rage_engine.pam_p, tx->rdb->volatile_rage_engine.pmm_p, transaction_id, &abort_error_dummy);
+	}
+}
+
 void deinitialize_transaction(transaction* tx)
 {
-	tx->db = NULL;
+	for(uint32_t i = 0; i < TEMPORARY_EXTENSION_STORE_COUNT; i++)
+	{
+		const void* transaction_id = NULL;
+		int abort_error_dummy = 0;
+		destroy_blob_store(tx->temp_ext_stores[i].blob_store_root_page_id, &(tx->rdb->volatile_rage_engine.bstd), tx->rdb->volatile_rage_engine.pam_p, transaction_id, &abort_error_dummy);
+		deinitialize_rwlock(&(tx->temp_ext_stores[i].blob_store_lock));
+	}
+
+	tx->rdb = NULL;
 	tx->snapshot = NULL;
 	tx->transaction_id = NULL;
-	tx->curr_query = NULL;
 }
