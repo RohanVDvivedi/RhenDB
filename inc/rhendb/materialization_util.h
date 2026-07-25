e595687758de17507920e382ba9c88ee6334ac2e
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-25 17:34:07 +0530

    added utility functions for the materialization of text blob and numeric

--- inc/rhendb/materialization_util.h
+++ inc/rhendb/materialization_util.h
@@ -1,20 +1,30 @@
 #ifndef MATERIALIZATION_UTIL_H
 #define MATERIALIZATION_UTIL_H
 
+#include<rhendb/transaction.h>
+
+#include<tuplelargetypes/text_extended.h>
+#include<tuplelargetypes/blob_extended.h>
+#include<tuplelargetypes/numeric_extended.h>
+
+#include<mpdecimal.h>
+
 /*
 	this utility materializes the text/blob into char* and numeric into mpd_t
-	if you 
+	if you are materializing something from the persistent_acid_storage_engine make sure you have atleast 1 buffer from the bufferpool available
 */
 
 // dti must be a text/blob type, inline or extended
-// directly returns NULL, if the dti is not a text or blob type
-char* materialize_tb(const datum* uval, const data_type_info* dti, transaction* tx, uint32_t* length, uint32_t* capacity);
+// directly returns NULL, if the dti is not a text or blob type, if dti is NULL, we expect uval to be a native tuple store string datum
+// uval input parameter for this function must be not a NULL_DATUM
+char* materialize_tb(const datum uval, const data_type_info* dti, transaction* tx, uint32_t* length, uint32_t* capacity);
 
 #define materialize_text materialize_tb
 #define materialize_blob materialize_tb
 
 // dti must be a numeric type, inline or extended
 // directly returns NAN, if the dti is not a numeric type
-mpd_t materialize_numeric(const datum* uval, const data_type_info* dti, transaction* tx);
+// uval input parameter for this function must be not a NULL_DATUM
+mpd_t materialize_numeric(const datum uval, const data_type_info* dti, transaction* tx);
 
 #endif
