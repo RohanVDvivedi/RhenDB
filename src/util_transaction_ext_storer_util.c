b44c262a515f488a2a09701367caf7eb852e4e28
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-25 22:35:29 +0530

    added a bytes writer for a fully materialized value for the project

--- src/util_transaction_ext_storer_util.c
+++ src/util_transaction_ext_storer_util.c
@@ -2,7 +2,65 @@
 
 #include<stdlib.h>
 
-void* tx_temp_store_tb(char* data, uint32_t data_size, data_type_info* ext_type_info, transaction* tx);
+void* tx_temp_store_tb(char* data, uint32_t data_size, data_type_info* ext_type_info, transaction* tx)
+{
+	tuple_def output_tuple_def;
+	initialize_tuple_def(&output_tuple_def, ext_type_info);
+
+	void* output_buffer = malloc(ext_type_info->max_size);
+	init_tuple(&output_tuple_def, output_buffer);
+
+	{
+		int abort_error_dummy = 0;
+		rage_engine* ex_engine = &(tx->rdb->volatile_rage_engine);
+
+		binary_write_iterator* wr = get_new_binary_write_iterator(output_buffer, &output_tuple_def, SELF, 0 /*dummy root*/, get_NULL_tuple_pointer(&(ex_engine->pam_p->pas)), ex_engine->max_prefix_size_in_bytes, &(ex_engine->bstd), ex_engine->pam_p, ex_engine->pmm_p);
+
+		temporary_extension_store* temp_ext_store = NULL;
+		uint32_t bytes_written = 0;
+
+		// write just the prefix
+		while(bytes_written < data_size && wr->bytes_written_to_prefix < wr->bytes_to_be_written_to_prefix)
+		{
+			uint32_t bytes_to_write_this_iteration = min(wr->bytes_to_be_written_to_prefix - wr->bytes_written_to_prefix, data_size - bytes_written);
+			uint32_t bytes_written_this_iteration = append_to_binary_write_iterator(wr, data + bytes_written, bytes_to_write_this_iteration, NULL, NULL, &abort_error_dummy);
+			if(bytes_written_this_iteration == 0)
+				break;
+			bytes_written += bytes_written_this_iteration;
+		}
+
+		// write the extension
+		if(bytes_written < data_size)
+		{
+			temp_ext_store = &(tx->temp_ext_stores[hash_element_within_tuple(output_buffer, &output_tuple_def, EXTENDED_PREFIX_POS_ACC, FNV_64_TUPLE_HASHER) % TEMPORARY_EXTENSION_STORE_COUNT]);
+
+			// set the write iterator with new root_page_id and take the lock before any further access
+			write_lock(&(temp_ext_store->blob_store_lock), BLOCKING);
+			wr->blob_store_root_page_id = temp_ext_store->blob_store_root_page_id;
+
+			const heap_table_notifier* htan_p = &HEAP_TABLE_ACCUMULATIVE_NOTIFIER(&(temp_ext_store->htan));
+
+			while(bytes_written < data_size)
+			{
+				uint32_t bytes_written_this_iteration = append_to_binary_write_iterator(wr, data + bytes_written, data_size - bytes_written, htan_p, NULL, &abort_error_dummy);
+				if(bytes_written_this_iteration == 0)
+					break;
+				bytes_written += bytes_written_this_iteration;
+
+				fix_unused_space_entries_in_store(tx, temp_ext_store);
+			}
+		}
+
+		delete_binary_write_iterator(wr, NULL, &abort_error_dummy);
+		if(temp_ext_store != NULL)
+		{
+			fix_unused_space_entries_in_store(tx, temp_ext_store);
+			write_unlock(&(temp_ext_store->blob_store_lock));
+		}
+	}
+
+	return output_buffer;
+}
 
 void* tx_temp_store_numeric(mpd_t* number, data_type_info* ext_type_info, transaction* tx)
 {
