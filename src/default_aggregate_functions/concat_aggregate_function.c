5c0c1e4992c8102697dfc7ba6d7caac7211d3433
RohanVDvivedi <rohandvivedi@gmail.com>
2026-09-01 21:30:30 +0530

    concat aggregation function implemented

--- src/default_aggregate_functions/concat_aggregate_function.c
+++ src/default_aggregate_functions/concat_aggregate_function.c
@@ -18,6 +18,8 @@ struct concat_context
 	dstring delimeter;
 
 	dstring suffix;
+
+	tuple_def output_tuple_def;
 };
 
 typedef struct concat_state concat_state;
@@ -30,6 +32,9 @@ struct concat_state
 	void* result; // this will be an extended text or blob in volatile page store
 
 	tuple_pointer tail_pointer;
+
+	// temp ext_store it belongs to, NULL if not yet known
+	temporary_extension_store* temp_ext_store;
 };
 
 static concat_state* create_concat_state(const aggregate_function* af_p)
@@ -41,14 +46,79 @@ static concat_state* create_concat_state(const aggregate_function* af_p)
 	state->bytes_appended = 0;
 	state->result = malloc(af_p->output_type_info->max_size);
 	state->tail_pointer = get_NULL_tuple_pointer(&(cc->tx->rdb->volatile_rage_engine.pam_p->pas));
+	state->temp_ext_store = NULL;
 
 	return state;
 }
 
 static int append_bytes(const aggregate_function* af_p, concat_state* state, const void* data, uint64_t data_size)
 {
+	// fail if the bytes count can not be held in 32 bit unsigned integer
+	if(data_size > UINT32_MAX)
+		return 0;
+	if(state->bytes_appended + data_size > UINT32_MAX)
+		return 0;
+
 	const concat_context* cc = af_p->context_p;
-	return 0;
+
+	rage_engine* ex_engine = &(cc->tx->rdb->volatile_rage_engine);
+
+	binary_write_iterator* wr = get_new_binary_write_iterator(state->result, &(cc->output_tuple_def), SELF, 0 /*dummy root*/, state->tail_pointer, ex_engine->max_prefix_size_in_bytes, &(ex_engine->bstd), ex_engine->pam_p, ex_engine->pmm_p);
+
+	int abort_error_dummy = 0;
+
+	{
+		uint32_t bytes_written = 0;
+		do
+		{
+			if(state->temp_ext_store == NULL)
+			{
+				// write just the prefix
+				while(bytes_written < data_size && wr->bytes_written_to_prefix < wr->bytes_to_be_written_to_prefix)
+				{
+					uint32_t bytes_to_write_this_iteration = min(wr->bytes_to_be_written_to_prefix - wr->bytes_written_to_prefix, data_size - bytes_written);
+					uint32_t bytes_written_this_iteration = append_to_binary_write_iterator(wr, data + bytes_written, bytes_to_write_this_iteration, NULL, NULL, &abort_error_dummy);
+					if(bytes_written_this_iteration == 0)
+						break;
+					bytes_written += bytes_written_this_iteration;
+					state->bytes_appended += bytes_written_this_iteration;
+				}
+
+				if(state->bytes_appended == ex_engine->max_prefix_size_in_bytes)
+					state->temp_ext_store = &(cc->tx->temp_ext_stores[hash_element_within_tuple(state->result, &(cc->output_tuple_def), EXTENDED_PREFIX_POS_ACC, FNV_64_TUPLE_HASHER) % TEMPORARY_EXTENSION_STORE_COUNT]);
+			}
+			else
+			{
+				write_lock(&(state->temp_ext_store->blob_store_lock), BLOCKING);
+				wr->blob_store_root_page_id = state->temp_ext_store->blob_store_root_page_id;
+
+				const heap_table_notifier* htan_p = &HEAP_TABLE_ACCUMULATIVE_NOTIFIER(&(state->temp_ext_store->htan));
+
+				while(bytes_written < data_size)
+				{
+					uint32_t bytes_written_this_iteration = append_to_binary_write_iterator(wr, data + bytes_written, data_size - bytes_written, htan_p, NULL, &abort_error_dummy);
+					if(bytes_written_this_iteration == 0)
+						break;
+					bytes_written += bytes_written_this_iteration;
+					state->bytes_appended += bytes_written_this_iteration;
+
+					fix_unused_space_entries_in_store(cc->tx, state->temp_ext_store);
+				}
+			}
+		}
+		while(bytes_written < data_size);
+	}
+
+	state->tail_pointer = wr->extension_tail;
+	delete_binary_write_iterator(wr, NULL, &abort_error_dummy);
+
+	if(state->temp_ext_store != NULL)
+	{
+		fix_unused_space_entries_in_store(cc->tx, state->temp_ext_store);
+		write_unlock(&(state->temp_ext_store->blob_store_lock));
+	}
+
+	return 1;
 }
 
 static int process_input(const aggregate_function* af_p, void** state_p, const datum inputs[])
@@ -162,6 +232,8 @@ aggregate_function* get_concat_aggregate_function(transaction* tx, const data_ty
 	af_p->destroy_aggregate_function = destroy_aggregate_function;
 
 	af_p->output_type_info = is_text_type_info(input_type_info) ? tx->rdb->volatile_rage_engine.text_extended_type_info : tx->rdb->volatile_rage_engine.blob_extended_type_info;
+
+	initialize_tuple_def(&(((concat_context*)(af_p->context_p))->output_tuple_def), (data_type_info*)af_p->output_type_info);
 
 	af_p->buffers_resource_count = has_extended_type_info(input_type_info, PERSISTENT_EXT_SUB_TYPE); // only the case with extended numeric to be added
 
