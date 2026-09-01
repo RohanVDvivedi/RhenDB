dcacff73d0f48a670a3c2e17cd565d62747bb9c9
RohanVDvivedi <rohandvivedi@gmail.com>
2026-09-01 14:37:20 +0530

    integration of hash with union type

--- src/function_hash.c
+++ src/function_hash.c
@@ -43,6 +43,19 @@ uint64_t hash_datum_rhendb(const datum* uval, const data_type_info* dti, tuple_h
 		return th->hash;
 	else if(!is_container_type_info(dti)) // non container types, primitive numbers: bit_field, uint, int, large_uint, large_int, float
 		return hash_datum(uval, dti, th);
+	else if(is_unified_type_info(dti)) // if it is unified dti, iterate overall all its components and return the hash value of the first non NULL component
+	{
+		for(uint32_t i = 0; i < get_element_count_for_datum(uval, dti); i++)
+		{
+			const data_type_info* child_dti;
+			datum child_value;
+			if(!get_containee_from_datum(&child_value, &child_dti, uval, dti, i))
+				continue;
+			if(!is_datum_NULL(&child_value)) // if the child is non-NULL, return it's hash
+				return hash_datum_rhendb(&child_value, child_dti, th, tx); // nestedly hash it using the very same function
+		}
+		return th->hash; // fall back return 
+	}
 	else if(is_text_type_info(dti) || is_blob_type_info(dti) || is_numeric_type_info(dti)) // all string/blob and numeric types
 	{
 		// for implicit read only transactions for accessing extended types in blob_store
