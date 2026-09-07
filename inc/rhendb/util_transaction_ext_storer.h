b06f1fbce338f8fc087f5facffc972ab7168327d
RohanVDvivedi <rohandvivedi@gmail.com>
2026-09-07 18:51:32 +0530

    minor addition to the comments

--- inc/rhendb/util_transaction_ext_storer.h
+++ inc/rhendb/util_transaction_ext_storer.h
@@ -13,9 +13,9 @@
 	this utility sotres the text/blob into transaction's extension_store
 */
 
-// ext_type_info is always expected to be volatile_rage_engine->blob/text_extended_type_info
+// ext_type_info is always expected to be volatile_rage_engine->blob/text/jsonb_extended_type_info
 // data must not be NULL, and is left owned by the caller
-// returned pointer if not NULL must be freed
+// returned pointer if not NULL must be freed, after setting it into the tuple
 void* tx_temp_store_tb(const char* data, uint32_t data_size, const data_type_info* ext_type_info, transaction* tx);
 
 #define tx_temp_store_text tx_temp_store_tb
@@ -23,7 +23,7 @@ void* tx_temp_store_tb(const char* data, uint32_t data_size, const data_type_inf
 
 // ext_type_info is always expected to be volatile_rage_engine->numeric_extended_type_info
 // number must not be NULL, and is left owned by the caller
-// returned pointer if not NULL must be freed
+// returned pointer if not NULL must be freed, after setting it into the tuple
 void* tx_temp_store_numeric(mpd_t* number, const data_type_info* ext_type_info, transaction* tx);
 
 #endif
